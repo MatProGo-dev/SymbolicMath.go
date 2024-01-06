aa3626f583af32d170813ef51b40dc5541fae14f
Kwesi Rutledge <kwesir@csail.mit.edu>
2024-01-06 16:46:06 -0500

    Added Tests for VectorConstraint object

--- testing/symbolic/vector_constraint_test.go
+++ testing/symbolic/vector_constraint_test.go
@@ -7,7 +7,10 @@ Description:
 */
 
 import (
+	"fmt"
+	"github.com/MatProGo-dev/SymbolicMath.go/smErrors"
 	"github.com/MatProGo-dev/SymbolicMath.go/symbolic"
+	"strings"
 	"testing"
 )
 
@@ -63,4 +66,187 @@ func TestVectorConstraint_Check2(t *testing.T) {
 			err.Error(),
 		)
 	}
+}
+
+/*
+TestVectorConstraint_Dims1
+Description:
+
+	This function tests that a vector constraint composed of two length 10 vectors
+	returns dims of (10, 1).
+*/
+func TestVectorConstraint_Dims1(t *testing.T) {
+	// Constants
+	N := 10
+	left := symbolic.KVector(symbolic.OnesVector(N))
+	right := symbolic.NewVariableVector(N)
+
+	// Test
+	vc := symbolic.VectorConstraint{left, right, symbolic.SenseLessThanEqual}
+	dims := vc.Dims()
+	if dims[0] != 10 || dims[1] != 1 {
+		t.Errorf(
+			"Expected vc.Dims() to return [10, 1]; received %v",
+			dims,
+		)
+	}
+}
+
+/*
+TestVectorConstraint_Dims2
+Description:
+
+	This test verifies that the Dims() method panics if the VectorConstraint is improperly
+	defined.
+*/
+func TestVectorConstraint_Dims2(t *testing.T) {
+	// Constants
+	N := 10
+	left := symbolic.KVector(symbolic.OnesVector(N))
+	right := symbolic.NewVariableVector(N + 1)
+
+	// Test
+	vc := symbolic.VectorConstraint{left, right, symbolic.SenseLessThanEqual}
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf(
+				"Expected vc.Dims() to panic; received nil",
+			)
+		}
+
+		rAsError := r.(error)
+		if rAsError.Error() != (fmt.Errorf(
+			"Left hand side has dimension %v, but right hand side has dimension %v!",
+			vc.LeftHandSide.Len(),
+			vc.RightHandSide.Len(),
+		)).Error() {
+			t.Errorf(
+				"Expected vc.Dims() to panic with error \"Left hand side has dimension %v, but right hand side has dimension %v!\"; received \"%v\"",
+				vc.LeftHandSide.Len(),
+				vc.RightHandSide.Len(),
+				rAsError.Error(),
+			)
+		}
+	}()
+	vc.Dims()
+}
+
+/*
+TestVectorConstraint_AtVec1
+Description:
+
+	This function tests that the AtVec method returns the correct scalar constraint
+	for a vector constraint of dimension (7, 1).
+*/
+func TestVectorConstraint_AtVec1(t *testing.T) {
+	// Constants
+	N := 7
+	left := symbolic.KVector(symbolic.OnesVector(N))
+	right := symbolic.NewVariableVector(N)
+	vc := symbolic.VectorConstraint{left, right, symbolic.SenseLessThanEqual}
+
+	// Test
+	for i := 0; i < N; i++ {
+		sc := vc.AtVec(i)
+		if sc.LeftHandSide.Dims()[0] != 1 || sc.RightHandSide.Dims()[0] != 1 || sc.Sense != symbolic.SenseLessThanEqual {
+			t.Errorf(
+				"Expected vc.AtVec(%v) to return a scalar constraint with LHS = 1, RHS = 1, and Sense = SenseLessThanEqual; received %v",
+				i,
+				sc,
+			)
+		}
+
+		lhsAsK := sc.LeftHandSide.(symbolic.K)
+		if float64(lhsAsK) != 1 {
+			t.Errorf(
+				"Expected vc.AtVec(%v).LeftHandSide to be 1; received %v",
+				i,
+				lhsAsK,
+			)
+		}
+	}
+}
+
+/*
+TestVectorConstraint_AtVec2
+Description:
+
+	This function tests that the AtVec method panics if the index is out of bounds.
+*/
+func TestVectorConstraint_AtVec2(t *testing.T) {
+	// Constants
+	N := 7
+	left := symbolic.KVector(symbolic.OnesVector(N))
+	right := symbolic.NewVariableVector(N)
+	vc := symbolic.VectorConstraint{left, right, symbolic.SenseLessThanEqual}
+
+	// Test
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf(
+				"Expected vc.AtVec(%v) to panic; received nil",
+				N,
+			)
+		}
+
+		rAsError := r.(error)
+		expectedError := smErrors.OutOfBoundsError{Index: N, Expression: vc}
+		if !strings.Contains(
+			rAsError.Error(),
+			expectedError.Error(),
+		) {
+			t.Errorf(
+				"Expected vc.AtVec(%v) to panic with error \"Index %v is out of bounds for vector constraint of length %v\"; received \"%v\"",
+				N,
+				N,
+				N,
+				rAsError.Error(),
+			)
+		}
+	}()
+	vc.AtVec(N)
+}
+
+/*
+TestVectorConstraint_AtVec3
+Description:
+
+	This test verifies that the AtVec method panics if the index is fine but the
+	VectorConstraint is improperly defined.
+*/
+func TestVectorConstraint_AtVec3(t *testing.T) {
+	// Constants
+	N := 7
+	left := symbolic.KVector(symbolic.OnesVector(N))
+	right := symbolic.NewVariableVector(N + 1)
+	vc := symbolic.VectorConstraint{left, right, symbolic.SenseLessThanEqual}
+
+	// Test
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Errorf(
+				"Expected vc.AtVec(%v) to panic; received nil",
+				N,
+			)
+		}
+
+		rAsError := r.(error)
+		if rAsError.Error() != (fmt.Errorf(
+			"Left hand side has dimension %v, but right hand side has dimension %v!",
+			vc.LeftHandSide.Len(),
+			vc.RightHandSide.Len(),
+		)).Error() {
+			t.Errorf(
+				"Expected vc.Dims() to panic with error \"Left hand side has dimension %v, but right hand side has dimension %v!\"; received \"%v\"",
+				vc.LeftHandSide.Len(),
+				vc.RightHandSide.Len(),
+				rAsError.Error(),
+			)
+		}
+	}()
+
+	vc.AtVec(N - 1)
 }
