b41242128e138822f2e70ffd48b4cde64ac5447c
Kwesi Rutledge <kwesir@csail.mit.edu>
2024-01-17 11:05:41 -0500

    Introduced New Out Of Bounds Method for Matrices

--- smErrors/out_of_bounds_error.go
+++ smErrors/out_of_bounds_error.go
@@ -8,17 +8,34 @@ Description:
 	Defining the out of bounds error object and all of its associated functions.
 */
 
-type OutOfBoundsError struct {
+type InvalidVectorIndexError struct {
 	Index      int
 	Expression MatrixLike
 }
 
-func (obe OutOfBoundsError) Error() string {
+type InvalidMatrixIndexError struct {
+	RowIndex   int
+	ColIndex   int
+	Expression MatrixLike
+}
+
+func (ivi InvalidVectorIndexError) Error() string {
 	errOut := fmt.Sprintf(
 		"out of bounds error: index %v is out of bounds for %T object of dimension %v",
-		obe.Index,
-		obe.Expression,
-		obe.Expression.Dims(),
+		ivi.Index,
+		ivi.Expression,
+		ivi.Expression.Dims(),
+	)
+	return errOut
+}
+
+func (imi InvalidMatrixIndexError) Error() string {
+	errOut := fmt.Sprintf(
+		"out of bounds error: index (%v, %v) is out of bounds for %T object of dimension %v",
+		imi.RowIndex,
+		imi.ColIndex,
+		imi.Expression,
+		imi.Expression.Dims(),
 	)
 	return errOut
 }
@@ -26,10 +43,39 @@ func (obe OutOfBoundsError) Error() string {
 // Explicit Functions
 func CheckIndexOnVector(index int, vector MatrixLike) error {
 	if index < 0 || index >= vector.Dims()[0] {
-		return OutOfBoundsError{
+		return InvalidVectorIndexError{
 			Index:      index,
 			Expression: vector,
 		}
 	}
+	return nil
+}
+
+/*
+CheckIndexOnMatrix
+Description:
+
+	Checks that the index is valid for the matrix.
+*/
+func CheckIndexOnMatrix(rowIndex int, colIndex int, matrix MatrixLike) error {
+	// Check that row index is in bounds
+	if rowIndex < 0 || rowIndex >= matrix.Dims()[0] {
+		return InvalidMatrixIndexError{
+			RowIndex:   rowIndex,
+			ColIndex:   colIndex,
+			Expression: matrix,
+		}
+	}
+
+	// Check that column index is in bounds
+	if colIndex < 0 || colIndex >= matrix.Dims()[1] {
+		return InvalidMatrixIndexError{
+			RowIndex:   rowIndex,
+			ColIndex:   colIndex,
+			Expression: matrix,
+		}
+	}
+
+	// Otherwise, return nil
 	return nil
 }
