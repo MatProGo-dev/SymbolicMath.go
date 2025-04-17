61933f8a07412496ec7a8f71675b5590ee0d0ee7
Kwesi Rutledge <kwesir@csail.mit.edu>
2025-04-16 21:18:50 -0400

    Adding a new test for performing comparisons between a mat.VecDense and a PolynomialVector

--- testing/symbolic/polynomial_vector_test.go
+++ testing/symbolic/polynomial_vector_test.go
@@ -1816,6 +1816,40 @@ func TestPolynomialVector_Comparison4(t *testing.T) {
 }
 
 /*
+TestPolynomialVector_Comparison5
+Description:
+
+	This test verifies that the comparison function returns a well-defined constraint
+	when a PolynomialVector is compared with a mat.VecDense object.
+*/
+func TestPolynomialVector_Comparison5(t *testing.T) {
+	// Constants
+	pv1 := symbolic.PolynomialVector{}
+	for ii := 0; ii < 20; ii++ {
+		pv1 = append(pv1, symbolic.NewVariable().ToPolynomial())
+	}
+	v2 := symbolic.OnesVector(20)
+
+	// Test
+	comp := pv1.Comparison(v2, symbolic.SenseGreaterThanEqual)
+	vectorComparison1, tf := comp.(symbolic.VectorConstraint)
+	if !tf {
+		t.Errorf(
+			"Expected comp to be of type VectorConstraint; received %T",
+			comp,
+		)
+	}
+
+	// Check that the right hand side of the constraint has the length of 20.
+	if vectorComparison1.RightHandSide.Len() != 20 {
+		t.Errorf(
+			"Expected vectorComparison1.RightHandSide.Len() to be 20; received %v",
+			vectorComparison1.RightHandSide.Len(),
+		)
+	}
+}
+
+/*
 TestPolynomialVector_LessEq1
 Description:
 
